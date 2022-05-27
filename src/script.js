bee4a560538c53b1608fccc2359b693bbcb4b420
summerleekh <summerleekh@gmail.com>
2022-05-28 00:29:51 +0800

    Fixed API url for weather forecast

--- src/script.js
+++ src/script.js
@@ -90,7 +90,8 @@ function displayCelsiusTemperature(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast-element");
 
   let forecastHTML = `<div class="row">`;
@@ -124,11 +125,11 @@ function displayForecast() {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "73de22d96ffc7ddbc691a26d0f970281";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat={coordinates.lat}&lon={coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
-}
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
-displayForecast();
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 
 let celsiusTemperature = null;
 
